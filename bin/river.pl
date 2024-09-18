d675a7b665b70f8f96f32b8f71f19f14e7beddd0
Mickey Nasriachi <mickey@cpan.org>
2024-09-18 07:51:23 +0200

    cpan is the default index

--- bin/river.pl
+++ bin/river.pl
@@ -14,7 +14,7 @@ use MetaCPAN::Ingest qw< read_url >;
 my $river_url //= 'https://neilb.org/river-of-cpan.json.gz';
 my $river_data = decode_json( read_url($river_url) );
 
-my $es   = MetaCPAN::ES->new( index => "cpan", type => "distribution" );
+my $es   = MetaCPAN::ES->new( type => "distribution" );
 my $bulk = $es->bulk();
 
 log_info {'Updating the distribution index'};
