2024cb78e8718178b2a008ec9aa579b96a794a4a
Mickey Nasriachi <mickey@cpan.org>
2025-05-06 16:49:26 +0000

    checksum - fix script

--- bin/checksum.pl
+++ bin/checksum.pl
@@ -11,14 +11,14 @@ use MetaCPAN::Ingest qw< cpan_dir >;
 
 # args
 my $limit = 1000;
-my $dry_run;
+my ( $dry_run );
 GetOptions(
-    "limit=i" => \$limit,
     "dry_run" => \$dry_run,
+    "limit=i" => \$limit,
 );
 
 # setup
-my $es = MetaCPAN::ES->new( type => "release" );
+my $es = MetaCPAN::ES->new( index => "release" );
 my $bulk;
 $bulk = $es->bulk() unless $dry_run;
 
