7577cc67b217bd8776335745a2afcaa4e30dedf0
Mickey Nasriachi <mickey@cpan.org>
2026-04-23 11:33:50 +0200

    mapping: user confirmation before deleting indices

--- bin/mapping.pl
+++ bin/mapping.pl
@@ -3,6 +3,7 @@ use warnings;
 
 use Getopt::Long;
 use MetaCPAN::Mapper;
+use MetaCPAN::Ingest qw< are_you_sure >;
 
 my ( $index, $cmd );
 GetOptions(
@@ -13,14 +14,15 @@ die "cmd can only be one of: 'create', 'delete'\n"
     unless grep { $cmd eq $_ } qw< create delete >;
 
 # setup
-my $type = $index;
 
 my $mapper = MetaCPAN::Mapper->new();
 
-$mapper->index_delete($index)
-    if $mapper->index_exists($index);
+if ( $mapper->index_exists($index) ) {
+    are_you_sure("This action will delete index: $index");
+    $mapper->index_delete($index)
+}
 
 if ( $cmd eq 'create' ) {
     $mapper->index_create($index);
-    $mapper->index_add_mapping( $index, $type );
+    $mapper->index_add_mapping($index);
 }
